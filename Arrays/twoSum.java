73a52952087accc61a2ba3df37abe92e15cea6fa
isaaccho <cho_isaac@hotmail.com>
2019-03-01 20:50:34 -0500

    finished twosum problem

---
+++ Arrays/twoSum.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        int len = nums.length;
+        int[] ans = new int[2];
+
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+
+                if (j == i) {
+                    j++;
+                    if (i == len - 1) {
+                        j--;
+                    }
+
+                    if (nums[i] + nums[j] == target) {
+                        ans[0] = i;
+                        ans[1] = j;
+                    }
+
+                }
+
+            }
+
+        }
+        return ans;
+    }
+}
