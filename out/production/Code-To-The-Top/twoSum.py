5a6caab577fa8ae2b0c90b8001fa0982657de60e
isaaccho <isaacshcho@gmail.com>
2019-06-04 14:22:34 -0400

    fixed error with intellij

--- out/production/Code-To-The-Top/twoSum.py
+++
@@ -1,9 +0,0 @@
-# testing for python
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        
