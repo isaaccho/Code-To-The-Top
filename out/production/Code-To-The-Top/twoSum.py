b09b3003f70fdd3269cfd017bd567299c6262014
isaaccho <isaacshcho@gmail.com>
2019-06-04 14:20:41 -0400

    deleted hash

---
+++ out/production/Code-To-The-Top/twoSum.py
@@ -0,0 +1,9 @@
+# testing for python
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
