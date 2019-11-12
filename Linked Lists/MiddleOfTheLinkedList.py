0adbdce3abe160a68aa3e0d4481220c2b553b727
isaaccho <isaacshcho@gmail.com>
2019-11-12 16:48:02 -0500

    finished middle of llist question

--- Linked Lists/MiddleOfTheLinkedList.py
+++ Linked Lists/MiddleOfTheLinkedList.py
@@ -1,33 +1,43 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode:
+     def __init__(self, x):
+         self.val = x
+         self.next = None
 
 class Solution:
 #    def length(self):
 
  #       return length
-   # def __init__(self):
-    #    self.head=ListNode(null)
+#    def __init__(self):
+ #       self.head=ListNode(self)
     
     def middleNode(self, head: ListNode) -> ListNode:
+        llist = head
         length = 0
-        cur_node=self.ListNode
-        while cur_node.next!=None:
+        cur_node=llist
+        while cur_node:
             length = length +1
             cur_node = cur_node.next
-            
+        print(length)    
     
-        ans= (length/2) 
-        cur_node = self.ListNode
-        del_node = cur_node
+        ans= ((length//2)) 
+       # if length == 1:
+       #     return llist
+       # elif length ==2:
+       #     return llist.next
+        result = llist
+        del_node = result
         index=0
-        while del_node.next !=None:
+        while del_node:
             if (ans==index):
-                cur_node.next=del_node
+                result=del_node
+                
+                return result
             del_node = del_node.next
-        return cur_node
+            index = index + 1
+       
         
+        
+
         
         
