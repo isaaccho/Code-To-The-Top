6a48e95d22c6e9598dcbd98fb0edb9b59f8dd4fc
isaaccho <isaacshcho@gmail.com>
2020-01-08 11:49:40 -0500

    pythonic solution

--- Strings/IPAddress.py
+++ Strings/IPAddress.py
@@ -1,16 +1,21 @@
 class Solution:
     def defangIPaddr(self, address: str) -> str:
-        res = address.split('.')
-        ans = ""
-        check = 0
-        a1 =len(res)
-        for x in res:
-            check = check + 1
-            if check == a1:
-                ans = ans + x
-                return ans
-            ans = ans + x + "[.]"
+        
+        # #brute force waym,O(n)
+        # res = address.split('.')
+        # ans = ""
+        # check = 0
+        # a1 =len(res)
+        # for x in res:
+        #     check = check + 1
+        #     if check == a1:
+        #         ans = ans + x
+        #         return ans
+        #     ans = ans + x + "[.]"
             
+        #one liner, O(n)
+        return address.replace(".","[.]")
+        
   
         
         
